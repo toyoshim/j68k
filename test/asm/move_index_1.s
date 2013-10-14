fa3c9307aa54143948b6b1b16771c9b6a617c6cc
Takashi Toyoshima <toyoshim@gmail.com>
2013-10-14 13:41:34 -0400

    add tests for move operations

---
+++ test/asm/move_index_1.s
@@ -0,0 +1,27 @@
+main:
+    lea data,%a0
+    moveq #0,%d0
+    movel %a0@(0,%d0:l:1),%d1
+    movel %a0@(4,%d0:l:1),%d2
+    moveq #4,%d0
+    movel %a0@(0,%d0:l:1),%d3
+    movel %a0@(0,%d0:l:2),%d4
+    moveq #1,%d0
+    movel %a0@(0,%d0:l:4),%d5
+    movel %a0@(0,%d0:l:8),%d6
+
+check:
+	.dc.l 0xffffffff
+	.dc.l 0xd0, 1
+	.dc.l 0xd1, 0x12345678
+	.dc.l 0xd2, 0xdeadbeaf
+	.dc.l 0xd3, 0xdeadbeaf
+	.dc.l 0xd4, 0x33333333
+	.dc.l 0xd5, 0xdeadbeaf
+	.dc.l 0xd6, 0x33333333
+	.dc.l 0
+
+data:
+    .dc.l 0x12345678
+    .dc.l 0xdeadbeaf
+    .dc.l 0x33333333
---
+++ test/asm/move_reg_1.s
@@ -0,0 +1,16 @@
+main:
+    lea data,%a0
+    movel %a0@,%a0
+    movel %a0,%d0
+    movel %d0,%d1
+
+check:
+	.dc.l 0xffffffff
+	.dc.l 0xa0, 0x12345678
+	.dc.l 0xd0, 0x12345678
+	.dc.l 0xd1, 0x12345678
+	.dc.l 0xf0f, 0
+	.dc.l 0
+
+data:
+    .dc.l 0x12345678
